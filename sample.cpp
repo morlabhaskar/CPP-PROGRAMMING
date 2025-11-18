36480619cb9f8ab8a1cf1ad5c778ce96737f1289
morlabhaskar <morlabhaskar306@gmail.com>
2025-11-18 21:40:41 +0530

    Add some files

---
+++ sample.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout << "Enter Number :" << endl;
+    cin >> num;
+    cout << "Entered Number is : " << num;
+    return 0;
+}
