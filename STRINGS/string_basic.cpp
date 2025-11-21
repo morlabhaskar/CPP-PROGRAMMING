f29fc2e113cc37d9c6ee0d53bf13d4acb4ead1d3
morlabhaskar <morlabhaskar306@gmail.com>
2025-11-21 23:15:56 +0530

    Add some Files

--- STRINGS/string_basic.cpp
+++ STRINGS/string_basic.cpp
@@ -15,7 +15,8 @@ using namespace std;
 int main(){
     string s1,s2;
     cout<<"Enter a String :"<<endl;
-    cin>>s1;
+    // cin>>s1;//cin considers a space (whitespace, tabs, etc) as a terminating character
+    getline(cin,s1);//function to read a line of text
     s2=s1;
     cout<<"s1 :"<<s1<<endl;
     cout<<"s2 :"<<s2<<endl;
