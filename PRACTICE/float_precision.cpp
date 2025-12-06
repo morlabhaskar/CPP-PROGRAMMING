7e45e0ce204fe31d6c304cdb826f44e982fc571f
morlabhaskar <morlabhaskar306@gmail.com>
2025-12-06 21:30:23 +0530

    Add files

---
+++ PRACTICE/float_precision.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float num;
+    cout<<"Enter a Number : "<<endl;
+    cin>>num;
+    cout<<fixed;
+    cout.precision(3);
+    cout<<num<<endl;
+}
