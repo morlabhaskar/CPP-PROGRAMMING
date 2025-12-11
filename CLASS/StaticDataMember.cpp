a05e2dea0a5cce7b1a7609ca7a8d657d3f738ea2
morlabhaskar <morlabhaskar306@gmail.com>
2025-12-11 22:40:38 +0530

    Add Files

---
+++ CLASS/StaticDataMember.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class ABC{
+	int data1,data2;
+	static int cnt;
+	public : 
+        ABC(int a=0,int b=0){
+		    cout<<"Constructor:"<<endl;
+		    data1=a;
+		    data2=b;
+		    cnt++;
+	    }
+		ABC(ABC &obj){
+			cout<<"Copy cons:"<<endl;
+			data1=obj.data1;
+			data2=obj.data2;
+			cnt++;
+		}
+		void Print(){
+			cout<<"data1:"<<data1<<endl;
+			cout<<"data2:"<<data2<<endl;
+			cout<<"current no of objs"<<cnt<<endl;
+		}
+};
+int ABC :: cnt;// defining the static data member outside the class (in  datasegment)
+int main(){
+	ABC obj1;
+	obj1.Print();
+	ABC obj2(10,20);
+	obj2.Print();
+
+	ABC obj3=obj1;
+	obj3.Print();
+}
