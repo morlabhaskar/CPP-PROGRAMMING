861145b035914b690cc3817f8761a888ce52ab94
morlabhaskar <morlabhaskar306@gmail.com>
2025-12-14 13:00:16 +0530

    Add Files

--- CLASS/friend/Faculty.cpp
+++ CLASS/friend/Faculty.cpp
@@ -8,3 +8,28 @@ Faculty :: Faculty(){
     cout<<"Enter the experience : "<<endl;
     cin>>exp;
 }
+Faculty :: Print(){
+    cout<<"EmpId : "<<empid<<endl;
+    cout<<"Name : "<<name<<endl;
+    cout<<"Subject : "<<subject<<endl;
+    cout<<"Experience : "<<exp<<endl;
+}
+void Faculty :: UpLoadMarks(Student &s){
+    s.totMarks = 0;
+    for(int i=0;i<6;i++){
+        cout<<"Enter Marks : "<<endl;
+        cin>>s.marks[i];
+        s.totMarks += s.marks[i];
+    }
+    s.per = s.totMarks/6;
+    if(s.per<40)
+		strcpy(s.grade,"C");
+	else if(s.per>=40 && S.per<50)
+		strcpy(s.grade,"B");
+	else if(s.per>=50 && s.per<60)
+		strcpy(S.grade,"B+");
+	else if(s.per>=60 && s.per<70)
+		strcpy(s.grade,"A");
+	else 
+		strcpy(s.grade,"A+");
+}
