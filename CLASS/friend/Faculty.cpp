fe4b7d19f4a53438e18ccb50c8d825fe74688cf3
morlabhaskar <morlabhaskar306@gmail.com>
2025-12-14 12:51:28 +0530

    Add Files

---
+++ CLASS/friend/Faculty.cpp
@@ -0,0 +1,10 @@
+Faculty :: Faculty(){
+    cout<<"Enter the empId : "<<endl;
+    cin>>empid;
+    cout<<"Enter the name : "<<endl;
+    cin>>name;
+    cout<<"Enter the subject : "<<endl;
+    cin>>subject;
+    cout<<"Enter the experience : "<<endl;
+    cin>>exp;
+}
