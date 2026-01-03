6f894d30bbdebe79470345561ea67a6882fc9ccf
morlabhaskar <morlabhaskar306@gmail.com>
2026-01-03 21:28:44 +0530

    Files Added

--- POLYMORPHISM/COMPILETIME POLYMORPHISM/FUNCTION OVERLOADING/FuncOVL1.cpp
+++ POLYMORPHISM/COMPILETIME POLYMORPHISM/FUNCTION OVERLOADING/FuncOVL1.cpp
@@ -1,29 +1,6 @@
 #include<iostream>
 using namespace std;
 
-void Input(int *,int);
-void Input(double *,int);
-
-void Print(int *,int);
-void Print(double *,int);
-
-void Rev(int *,int);
-void Rev(double *,int);
-
-int main(){
-	int a[5];
-	double d[7];
-	Input(a,5);
-	Print(a,5);
-	Rev(a,5);
-	Print(a,5);
-	
-	Input(d,7);
-	Print(d,7);
-	Rev(d,7);
-	Print(d,7);
-}
-
 void Input(int *p,int s){
 	int i;
 	for(i=0;i<s;i++){
@@ -31,14 +8,12 @@ void Input(int *p,int s){
 		cin>>p[i];
 	}
 }
-
 void Print(int *p,int s){
 	for(int i=0;i<s;i++){
 		cout<<p[i]<<",";
 	}
 	cout<<endl;
 }
-
 void Rev(int *p,int s){	
     int t;
 	for(int i=0,j=s-1;i<j;i++,j--){
@@ -47,7 +22,6 @@ void Rev(int *p,int s){
 		p[j]=t;
 	}
 }
-
 void Input(double *p,int s){
 	int i;
 	for(i=0;i<s;i++){
@@ -55,14 +29,12 @@ void Input(double *p,int s){
 		cin>>p[i];
 	}
 }
-
 void Print(double *p,int s){
 	for(int i=0;i<s;i++){
 		cout<<p[i]<<",";
 	}
 	cout<<endl;
 }
-
 void Rev(double *p,int s){	
     double t;
 	for(int i=0,j=s-1;i<j;i++,j--){
@@ -70,5 +42,18 @@ void Rev(double *p,int s){
 		p[i]=p[j];
 		p[j]=t;
 	}
+}
+int main(){
+	int a[5];
+	double d[5];
+	Input(a,5);
+	Print(a,5);
+	Rev(a,5);
+	Print(a,5);
+	
+	Input(d,5);
+	Print(d,5);
+	Rev(d,5);
+	Print(d,5);
 }
 
