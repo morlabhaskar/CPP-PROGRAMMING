26d0f11871a10a7f3dd284e4443c297d99d64a2a
morlabhaskar <morlabhaskar306@gmail.com>
2026-01-19 19:07:48 +0530

    Files Added

---
+++ DSA/LINKED LIST/CIRCULAR/scll_findLength.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int val;
+    struct Node* next;
+};
+void insertEnd(struct Node* &head,int data){
+    struct Node* newNode=new Node();
+    newNode->val=data;
+    if(head==NULL){
+        head=newNode;
+        newNode->next=newNode;
+        return;
+    }
+    struct Node* temp=head;
+    while(temp->next != head){
+        temp=temp->next;
+    }
+    temp->next=newNode;
+    newNode->next=head;
+}
+void findLength(struct Node* &head){
+    int len=0;
+    if(head==NULL){
+        cout<<"Length : "<<len<<endl;
+        return;
+    }
+    len=1;
+    struct Node* temp=head->next;
+    while(temp!=head){
+        len++;
+        temp=temp->next;
+    }
+    cout<<"Length : "<<len<<endl;
+}
+void displayNodes(struct Node* &head){
+    if(!head){
+        cout<<"List is Empty"<<endl;
+        return;
+    }
+    struct Node* temp=head;
+    cout<<"Circular Singly Linked List is :"<<endl;
+    do{
+        cout<<temp->val<<"->";
+        temp=temp->next;
+    }while(temp != head);
+    cout<<head->val<<endl;
+}
+int main(){
+    struct Node* head=NULL;
+    char ch;
+    while(1){
+        cout<<"i:insert  d:display  q:quit"<<endl;
+        cout<<"Enter the Choice :"<<endl;
+        cin>>ch;
+        switch(ch){
+            case 'i':
+                int data;
+                cout<<"Enter the Data :"<<endl;
+                cin>>data;
+                insertEnd(head,data);
+                break;
+            case 'd':
+                displayNodes(head);
+                break;
+            case 'f':
+                findLength(head);
+                break;
+            case 'q':
+                cout<<"Thank You"<<endl;
+                return 0;
+            default :
+                cout<<"Invalid Choice"<<endl;
+                break;
+        }
+    }
+}
