d38e85991d2680133e600fdcd9183ba8bce19e8e
morlabhaskar <morlabhaskar306@gmail.com>
2026-03-29 16:37:30 +0530

    Add STL:vector Problems

--- STL/containers/sequencial/vector/vec_rev.cpp
+++ STL/containers/sequencial/vector/vec_rev.cpp
@@ -4,21 +4,16 @@ using namespace std;
 int main(){
     vector<int> v;
     int n,x;
-    cout<<"Enter the size of Vector :"<<endl;
+    cout<<"Enter the Vector Size :"<<endl;
     cin>>n;
-    cout<<"Enter the Vector Elements :"<<endl;
     for(int i=0;i<n;i++){
         cin>>x;
         v.push_back(x);
     }
-    int max=v[0];
+    cout<<"Array Before Reverse :"<<endl;
     for(int i : v){
         cout<<i<<" ";
-        if(i>max){
-            max=i;
-        }
     }
     cout<<endl;
-    cout<<"Max Value : "<<max<<endl;
+    
 }
-//A 1 B 2 C 3
