99b112a8938c5baea0572ba4e10232ec6d2eb481
morlabhaskar <morlabhaskar306@gmail.com>
2026-09-13 17:09:28 +0530

    CMakeLists created
    Signed-off-by: morlabhaskar <morlabhaskar306@gmail.com>

---
+++ MAKE/build/CMakeFiles/MyApp.dir/math.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/MyApp.dir/math.cpp.obj: \
+ C:\Users\morla\OneDrive\Desktop\CPP\MAKE\math.cpp \
+ C:\Users\morla\OneDrive\Desktop\CPP\MAKE\math.h
---
+++ MAKE/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "math.h"
+using namespace std;
+int main(){
+    int a = 10, b = 20;
+    int res = add(a, b);
+    cout<<"Adding : "<<res<<endl;
+    return 0;
+}
